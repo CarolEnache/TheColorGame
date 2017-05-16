bcc6e9651259424513dc3bbae9120fdeb2591654
CarolEnache <enache.carol@gmail.com>
2017-05-16 17:05:35 +0100

    use Math.floor and Math.random to genearte random colors and shades

--- TheColorGame.js
+++ TheColorGame.js
@@ -1,11 +1,6 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
+
+
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
@@ -22,6 +17,7 @@ for (var i = 0; i < squares.length; i++) {
     // grab color of picked square
     var clickedColor = this.style.background;
     // compare color to pickedColor
+    console.log(clickedColor, pickedColor);
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
       changeColors(clickedColor);
@@ -45,4 +41,23 @@ function changeColors(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  // make an array
+  var arr = []
+    // add num random colors to array
+  for (i = 0; i < num; i++) {
+    arr.push(randomColor())
+  }
+  // return that array
+  return arr;
+}
+
+function randomColor() {
+  // pick a "red"
+  var r = Math.floor(Math.random() * 256)
+  var g = Math.floor(Math.random() * 256)
+  var b = Math.floor(Math.random() * 256)
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
